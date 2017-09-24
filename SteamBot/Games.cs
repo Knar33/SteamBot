578f2b6f42a949656bbd282132675fdb48ef3c61
Knar <adbyerly@comcast.net>
2017-09-24 14:03:09 -0500

    Initial Commit

---
+++ SteamBot/Games.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamBot
+{
+    public class Games
+    {
+        public Applist applist { get; set; }
+    }
+
+    public class Applist
+    {
+        public Apps[] apps { get; set; }
+    }
+
+    public class Apps
+    {
+        public string appid { get; set; }
+        public string name { get; set; }
+    }
+}
